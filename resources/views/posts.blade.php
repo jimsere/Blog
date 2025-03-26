399fee30134b61be774c563c1856f19142de1007
jimsere <dimitrisserefias@gmail.com>
2025-03-26 16:49:23 +0200

    second commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,48 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-     <!-- blog section -->
+<!-- blog section -->
+<section class="blog_section layout_padding">
+  <div class="container">
 
-  <section class="blog_section layout_padding">
-    <div class="container">
-      
-      @if (Auth::check())
-            <a href="{{route('newpost')}}">New Post</a>      
-      @endif
-      <div class="d-flex justify-content-center">
-        <form class="form-inline my-2 my-lg-0" method="GET" action="{{ route('search') }}">
-            <div class="row justify-content-center">
-                <div class="col-sm-8">
-                    <input class="form-control" type="search" placeholder="Αναζήτηση" aria-label="Search" name="q">
-                </div>
-                <div class="col-sm-4">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    
-    @foreach($posts as $post)
-    <div class="row">
-      <div class="col-md-6 col-lg-4 mx-auto">
-        <div class="box">
-          <div class="img-box">
-            <img src="images/b1.jpg" alt="">
+    @if (Auth::check())
+      <a href="{{route('newpost')}}">New Post</a>
+    @endif
+
+    <div class="d-flex justify-content-center mb-4">
+      <form class="form-inline my-2 my-lg-0" method="GET" action="{{ route('search') }}">
+        <div class="row justify-content-center">
+          <div class="col-sm-8">
+            <input class="form-control" type="search" placeholder="Αναζήτηση" aria-label="Search" name="q">
           </div>
-          <div class="detail-box">
-            <h5>
-              {{ $post->title }}
-            </h5>
-            <p>
-              {{ $post->body }}                 
-            </p>
-            <a href="{{ route('post', $post) }}">Read more</a>              
+          <div class="col-sm-4">
+            <button class="btn btn-outline-success" type="submit">Search</button>
           </div>
         </div>
-      </div>   
-  @endforeach        
-      </div>
+      </form>
     </div>
-  </section>
-  <!-- end blog section -->
+
+    <div class="row">
+      @foreach($posts as $post)
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-4">
+          <div class="box w-100">
+            <div class="img-box">
+              <img src="images/b1.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>{{ $post->title }}</h5>
+              <p>{{ $post->body }}</p>
+              <a href="{{ route('post', $post) }}">Read more</a>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+
+  </div>
+</section>
+<!-- end blog section -->
 @endsection
